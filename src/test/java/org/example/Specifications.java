527c0048f8b7020914af61dadff1decb03b67201
juliapertash <ameliadonaldduck@gmail.com>
2022-10-26 18:57:07 +0200

    Initial

---
+++ src/test/java/org/example/Specifications.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpecification(String url){
+        return new RequestSpecBuilder()
+                .setBasePath(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
